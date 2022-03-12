eb8489b9fcaef5971a53caca3f0db31b70b21055
rphukan <ranjan.phukan@gmail.com>
2022-03-12 14:35:41 +0530

    integrating

--- msr-documents-svc/run-int.sh
+++ msr-documents-svc/run-int.sh
@@ -1,1 +1,7 @@
-java -jar ./target/msr-documents-svc-0.0.1-SNAPSHOT.jar --spring.profiles.active=int --com.msr.eureka.url="https://localhost:8181/eureka" --com.msr.configserver.enabled=true
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+java -jar ./target/msr-documents-svc-0.0.1-SNAPSHOT.jar \
+--spring.profiles.active=int \
+--com.msr.eureka.url="https://localhost:8181/discovery/eureka" \
+--com.msr.configserver.enabled=true
